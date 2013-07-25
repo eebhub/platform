298e06509273fa85eca2775d3462e97789ee8730
nmattise <n.mattise@gmail.com>
2013-07-25 17:10:08 +0000

    Partial Data Dump

--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,5 @@
 var mit = require("../lib/mit.js");
+var fs = require("fs");
 
 module.exports = {
     getIndex: function(request, response){
@@ -23,10 +24,17 @@ module.exports = {
     },
     partialanalysis: function(request, response){
         var buildingName = request.body.building.building_name;
-        var buildingYear = request.body.building.year_completed;
-        console.log(buildingName);
-        console.log(buildingYear);
-        response.render('partial');
+        var stringBuilding = JSON.stringify(request.body.building);
+        var stringOccupancy = JSON.stringify(request.body.occupancy);
+        var stringMaterials = JSON.stringify(request.body.materials);
+        var stringMechanical = JSON.stringify(request.body.mechanical);
+        var stringRoom = JSON.stringify(request.body.room);
+        var string = stringBuilding  + '<br/>' + stringOccupancy  + '<br/>' + stringMaterials + '<br />' + stringMechanical + '<br />' + stringRoom;
+        var route = "./views/";
+        var fileName = route + buildingName + '.ejs' ;
+        fs.writeFileSync(fileName, string);
+        response.render(buildingName);
+        
     },
     testpost: function(request, response){
         var name = request.body.name;

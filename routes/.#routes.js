fb2ae1517446a6ce565e4bd1cd4f9b18460db0c4
nmattise <n.mattise@gmail.com>
2013-07-25 14:34:28 +0000

    Platform only index

---
+++ routes/.#routes.js
@@ -0,0 +1,1 @@
+ubuntu@ip-10-251-54-247.us-west-2.compute.internal.14562:1374678487
---
+++ routes/routes.js
@@ -0,0 +1,5 @@
+module.exports = {
+    getIndex: function(request, response){
+        response.render('index');    
+    },
+};

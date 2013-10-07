9998f576bb58911dac3490c952c1141589d5c214
OpenShift guest <y.sun1912@gmail.com>
2013-10-07 17:25:46 -0400

    test query

--- routes/routes.js
+++ routes/routes.js
@@ -31,27 +31,6 @@ module.exports = {
         response.sendfile('./views/comprehensive.html');
     },
        
-    
-    getRemoteEngineRun: function(request, response){
-        //var shell = require('shelljs');
-        shell.echo('hello world');
-        var str = shell.pwd();
-        shell.echo(str);
-        var nodeExec = shell.which('node');
-        shell.echo(nodeExec);
-        shell.exec('node --version', {silent:false}).output;
 
-        shell.exec('which node', {silent:false}).output;
-        
-        shell.exec('pwd', {silent:false}).output;
-        
-        shell.exec('echo hello', {silent:false}).output;
-        
-        shell.cd('lib');
-
-        shell.exec('pwd', {silent:false}).output;
-        
-        response.render('platform/remote');  
-    },
     
 };

c3a098628226c3370fc35d57f9ed2844ab124fda
OpenShift guest <y.sun1912@gmail.com>
2013-10-11 15:34:19 -0400

    add signin page

--- app.js
+++ app.js
@@ -10,10 +10,106 @@ var express = require('express')
   , lite = require("./routes/lite.js")
   , partial = require("./routes/partial.js")
   , substantial=require("./routes/substantial.js")
-  , shjs = require('shelljs/global');
+  , shjs = require('shelljs/global')
+  , mongoose = require('mongoose');
 
 
 var app = express();
+
+mongoose.connect("mongodb://128.118.67.242/test");
+var UserSchema = new mongoose.Schema({
+    username: String,
+    password: String,
+    salt: String,
+    hash: String
+});
+
+var User = mongoose.model('users', UserSchema);
+
+var BuildingSchema = new mongoose.Schema({
+    username: String,
+    building: {
+          building_info: {building_name: String,
+                          weather_epw_location: String,
+                          activity_type: String,
+                          activity_type_specific: String},
+          architecture:  {gross_floor_area: String,
+                          number_of_floors: String,
+                          window_to_wall_ratio: String,
+                          footprint_shape: String,
+                          building_height: String,
+                          perimeter: String},
+          materials:      {exterior_wall_type: String,
+                           window_glass_type: String,
+                           roof_type: String},
+            
+          people:         {number_of_employees_during_main_shift: String},
+          
+          mechanical:     {primary_hvac_type: String,
+                           electricity_used_for_main_heating: String,
+                           natural_gas_used_for_main_heating: String,
+                           fuel_oil_used_for_main_heating: String,
+                           propane_used_for_main_heating: String,
+                           district_steam_used_for_main_heating: String,
+                           district_hot_water_used_for_main_heating: String,
+                           electricity_used_for_cooling: String,
+                           natural_gas_used_for_cooling: String,
+                           fuel_oil_used_for_cooling: String,
+                           propane_used_for_cooling: String,
+                           district_steam_used_for_cooling: String,
+                           district_hot_water_used_for_cooling: String,
+                           district_chilled_water_used_for_cooling: String,
+                           electricity_used_for_water_heating: String,
+                           natural_gas_used_for_water_heating: String,
+                           fuel_oil_used_for_water_heating: String,
+                           propane_used_for_water_heating: String,
+                           district_steam_used_for_water_heating: String,
+                           district_hot_water_used_for_water_heating: String},
+                           
+          schedules:      {open_24_hours_a_day: String,
+                           average_weekly_operating_hours: String,
+                           open_during_week: String,
+                           open_on_weekend: String,
+                           weekday_occupancy_hours_day_start: String,
+                           weekday_occupancy_hours_day_end: String,
+                           saturday_occupancy_hours_day_start: String,
+                           saturday_occupancy_hours_day_end: String,
+                           sunday_occupancy_hours_day_start: String,
+                           sunday_occupancy_hours_day_end: String}
+          }                  
+   
+});
+
+var Building = mongoose.model('buildings', BuildingSchema);
+
+var BuildingModelSchema = new mongoose.Schema({
+    username: String,
+    model: String,                  
+   
+});
+
+var Buildingmodel = mongoose.model('buildingmodels', BuildingModelSchema);
+
+function authenticate(name, pass, fn) {
+    if (!module.parent) console.log('authenticating %s:%s', name, pass);
+
+    User.findOne({
+        username: name
+    },
+
+    function (err, user) {
+        if (user) {
+            if (err) return fn(new Error('cannot find user'));
+            
+                if (user.password == pass) return fn(null, user);
+                else return fn(new Error('invalid password'));
+           
+        } else {
+            return fn(new Error('cannot find user'));
+        }
+    });
+
+}
 
 // Seting Server environments, ports and rendering
 app.set('port', process.env.PORT || 3000);
@@ -21,6 +117,8 @@ app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
+app.use(express.cookieParser("secret"));
+app.use(express.session());
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
@@ -34,9 +132,11 @@ if ('development' == app.get('env')) {
 app.get('/', routes.getHome);
 app.get('/', routes.getHome);
 app.get('/signup', routes.getSignUp);
-app.get('/login', routes.getLogIn);
+app.get("/signin", function (req, res) {
+    res.render("signin");
+});
 app.get('/lite', routes.getLite);
-app.get('/liteconv', routes.getLiteConv);
+//app.get('/liteconv', routes.getLiteConv);
 app.get('/literesults', routes.getLiteResults);
 app.get('/partial', routes.getPartial);
 app.get('/substantial', routes.getSubstantial);
@@ -56,7 +156,29 @@ app.post('/substantialresults', substantial.getSubstantialResults);
 
 app.post('/imtanalysis', lite.runIMT);
 app.post('/ibmanalysis', lite.ibm);
-app.post('/lite-conversion-res', lite.runLiteConversion);
+
+app.post("/mydashboard", function (req, res) {
+    authenticate(req.body.username, req.body.password, function (err, user) {
+        if (user) {
+
+            req.session.regenerate(function () {
+
+                req.session.username = user.username;
+                //req.session.success = 'Authenticated as ' + user.username + ' click to <a href="/logout">logout</a>. ' + ' You may now access <a href="/restricted">/restricted</a>.';
+    //             res.render('substantialwelcome', {
+			 //       'username': req.body.username,     
+			 //   });
+			 
+			 res.send("welcome"+req.body.username);
+            });
+        } else {
+            req.session.error = 'Authentication failed, please check your ' + ' username and password.';
+            res.redirect('/signin');
+        }
+    });
+});
+
+
 
 
 
